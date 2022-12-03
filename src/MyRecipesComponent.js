2aeb796e92810c73c135e00752a109456eab0658
AlesjaScha <alesja310780@gmail.com>
2022-12-03 14:42:19 +0100

    first commit

---
+++ src/MyRecipesComponent.js
@@ -0,0 +1,29 @@
+
+function MyRecipesComponent({label,image,calories,ingredients}){
+    return(<div>
+
+          <div className="container">
+            <h2>{label}</h2>
+         </div>
+
+         <div className="container">
+            <img  className="tasty" src={image} alt="bild"/>
+        </div>
+
+        <ul className="list">
+        {ingredients.map((ingredient,index) => (
+            
+            <li key={index} > <img src= "https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/128/external-check-banking-and-finance-kiranshastry-gradient-kiranshastry.png"className="icon"alt="icon"/>  
+               {ingredient}</li>
+        ))}
+
+        </ul>
+
+        <div className="container">
+            <p className="par">{calories.toFixed()} calories</p>
+        </div>    
+
+        </div>)
+    
+}
+export default MyRecipesComponent;
